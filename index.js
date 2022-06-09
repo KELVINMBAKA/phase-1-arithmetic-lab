20a5e6002508fc206c5aee85cffd3afcd5281759
KELVINMBAKA <loverc832@gmail.com>
2022-06-09 21:56:58 +0300

    commit tree 21bade7069f949d2fd521899e383ab3414f2b30b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1=6.2
+const num2=10
+const multiply= (num1*num2)
+
+const random = Math.floor(Math.random() * 10 +1)
+ const num3=(60)
+ const num4=(7)
+ const mod=( num3%num4)
+const max=Math.max(4, 8, 9, 16, 20);
